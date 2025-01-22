6ff98771ffb4a60799d6b2df0c3c80db0a3cef17
Mahdi Abedini <m.abedini30@yahoo.com>
2025-01-22 13:37:33 +0330

    Set output leds

--- sim/src/src.ino
+++ sim/src/src.ino
@@ -7,7 +7,7 @@ const byte ledPins[] = {10, 11, 12, 13};
 const byte switchCount = INPUT_COUNT;
 const byte switchPins[] = {2, 3, 4, 5, 6, 7, 8, 9};
 
-unsigned circuit[STATE_COUNT] = {1};
+unsigned circuit[STATE_COUNT] = {0b1101}; // sample circuit: 00000000 -> 1101
 
 char buffer[200];
 
@@ -23,13 +23,15 @@ void setup() {
 }
 
 void loop() {
-  unsigned circuitState = 0;
+  unsigned inputState = 0;
   for (byte i = 0; i < switchCount; i++) {
     unsigned s = digitalRead(switchPins[i]) << i;
-    circuitState |= s;
+    inputState |= s;
   }
-  sprintf(buffer, "%u\n", circuitState);
-  Serial.print(buffer);
-  delay(1000);
+  unsigned outputState = circuit[inputState];
+  for (byte i = 0; i < ledCount; i++) {
+    bool s = (outputState >> i) & 1;
+    digitalWrite(ledPins[i], s);
+  }
 
 }
